687795188463b496b1d56aa1bde13f553c224504
Geancarlo Canova <geancanovawt@gmail.com>
2021-08-12 15:15:53 -0300

    OOP first challenge

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -71,3 +71,66 @@ const title = document.querySelector('h1');
 console.dir(title); // Prototype chain: HTMLHeadingElement => HTMLElement => Element => Node => EventTarget => object
 console.dir(x => x = 1); // Prototype contains functions methods
 
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed
+};
+
+Car.prototype.accelerate = function () {
+    console.log(`${this.make} is going at ${this.speed += 10} km/h`);
+};
+
+Car.prototype.brake = function (curSpeed = this.speed, curCar = this.make) {
+    curSpeed = this.speed -= 5;
+
+    if (curSpeed <= 0)
+        console.log(`${curCar} has stopped`);
+    else
+        console.log(`${curCar} is going at ${curSpeed} km/h`);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+console.log(car1);
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+
+console.log(car2);
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.accelerate();
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.brake();
+car2.brake();
+car2.brake();
+car2.brake();
+car2.brake();
+car2.brake();
