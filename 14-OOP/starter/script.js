87f7400ac5b07e10af5be70c921add3d324db091
Geancarlo Canova <geancanovawt@gmail.com>
2021-08-13 13:09:38 -0300

    ADD classes protected properties and methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -323,7 +323,7 @@ ford.accelerate();
 
 ///////////////////////////////////////
 // Inheritance Between "Classes": Constructor Functions
-
+/*
 const Person = function (firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
@@ -359,6 +359,7 @@ console.log(mike instanceof Person); // true
 console.log(mike instanceof Object); // true
 
 Student.prototype.constructor = Student;
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #3
@@ -373,6 +374,7 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
 */
+/*
 const Car = function (make, speed) {
     this.make = make;
     this.speed = speed
@@ -420,6 +422,191 @@ tesla.chargeBattery(90);
 console.log(tesla);
 tesla.accelerate();
 console.log(tesla);
+*/
 
-// EV.prototype.constructor = EV;
-// console.dir(EV);
+///////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+/*
+class PersonCl {
+    constructor(fullName, birthYear) {
+      this.fullName = fullName;
+      this.birthYear = birthYear;
+    }
+  
+    // Instance methods
+    calcAge() {
+      console.log(2037 - this.birthYear);
+    }
+  
+    greet() {
+      console.log(`Hey ${this.fullName}`);
+    }
+  
+    get age() {
+      return 2037 - this.birthYear;
+    }
+  
+    set fullName(name) {
+      if (name.includes(' ')) this._fullName = name;
+      else alert(`${name} is not a full name!`);
+    }
+  
+    get fullName() {
+      return this._fullName;
+    }
+  
+    // Static method
+    static hey() {
+      console.log('Hey there 👋');
+    }
+  }
+
+  class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        // Always need to happen first! (creates the 'this' keyword)
+        super(fullName, birthYear) // constructor
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+
+    calcAge() {
+        console.log(`I'm ${2037 - this.birthYear} years old, but as a student I feel more like ${2037 - this.birthYear + 10}`);
+    }
+  }
+
+  const martha = new StudentCl('Martha Jones', 2020, 'Medicine');
+  console.log(martha);
+  martha.introduce();
+  martha.calcAge();
+  */
+
+  ///////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+/*
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+  
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+  };
+  
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Design');
+jay.introduce();
+*/
+
+///////////////////////////////////////
+// Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+/*
+class Account {
+    // 1) Public fields (instances)
+    locale = navigator.language;
+    
+    // 2) Private fields
+    #movements = [];
+    #pin;
+
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        // protected properties (underscore as a convention for protected properties)
+        this.#pin = pin;
+
+        // Moved to private fields above 
+        // this._movements = [];
+
+        // Moved to public fields above 
+        // this.locale = navigator.language;
+
+        console.log(`Thanks for opening an account, ${owner}`)
+    }
+
+    // 3) Public methods
+    // Public interface (API)
+    getMovements() {
+        return this.#movements;
+    }
+
+    deposit(val) {
+        this.#movements.push(val);
+        return this;
+    }
+
+    withdraw(val) {
+        this.deposit(-val);
+        return this;
+    }
+
+    
+    requestLoan(val) {
+        // if (this.#approveLoan(val)) {
+        if (this._approveLoan(val)) {
+            this.deposit(val);
+            console.log(`Loan approved`);
+            return this;
+        }
+    }
+
+    // static methods will work only for this class, not for class instances
+    static helper() {
+        console.log('Helper');
+    }
+
+    // 4) Private methods
+    // #approveLoan(val) { (syntax on approval stage for private methods)
+    _approveLoan(val) {
+        return true;
+    }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+// acc1.approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+
+// Can't access a private field (Uncaught SyntaxError: Private field '#movements' must be declared in an enclosing class)
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+
+Account.helper();
+// acc1.helper(); Uncaught TypeError: acc1.helper is not a function at script.js:602
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
+*/
